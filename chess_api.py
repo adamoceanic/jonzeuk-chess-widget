46dfba0852b4b2219b5ad014d1c6b680160ab50a
Adam Jonze <ajze@users.noreply.github.com>
2018-03-20 01:28:33 +0000

    clickable move history

--- chess_api.py
+++ chess_api.py
@@ -1,21 +1,30 @@
+# boilerplate ideas
+# I want to take the work out of the javascript on my site and only have it query this API
+
 import requests
 import json
 from time import sleep
-from pprint import pprint as pp
 
 
 def getJson():
     url = 'https://api.chess.com/pub/player/ajze/games'
     r = requests.get(url)
-
     res = json.loads(r.content)
 
     for item in res['games']:
-
         if item['black'].find('anebir') != -1 or item['white'].find('anebir') != -1:
 
+        # do the things
 
-            pp(item['pgn'])
+
+def processData():
+
+
+def saveData():
+
+
+def sendToAPI():
+
 
 def start():
     while True:
